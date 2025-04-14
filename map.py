556202208b1dad7cd1eeb0cc6d5dfd07a923780a
Natalie Anderson <nateand@umich.edu>
2025-04-14 18:18:34 -0400

    Initial commit

--- map.py
+++ map.py
@@ -77,24 +77,4 @@ locations = [
         "city": "Prague, Czech Republic",
         "location": [50.0755, 14.4378],
         "blog": "blog/prague.html",
-        "icon": "images/location_icon.png"
-    },
-]
-
-for place in locations:
-    popup_html = f'<div style="font-size:16px;">My trip to <a href="{place["blog"]}" target="_blank" class=map_links >{place["city"]}</a>!</div>'
-    popup = folium.Popup(popup_html, max_width=500)
-    marker = folium.Marker(
-        location=place["location"],
-        popup=popup,
-        icon=create_custom_icon(place["icon"])
-    )
-    marker.add_to(my_map)
-
-# Add custom CSS
-css_link = '<link rel="stylesheet" type="text/css" href="css/style.css">'
-css_element = folium.Element(css_link)
-my_map.get_root().html.add_child(css_element)
-
-my_map.save("index.html")
-print("Map generated and saved as 'index.html'")
+        "icon": "images/location
